2a06349ffc7406b235a3a5690443d06d2a562f3e
Brian Marcel Patrao <patrao.b@northeastern.edu>
2024-02-20 21:37:59 -0500

    part1

---
+++ scripts/install_java.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "########## Installing Java 17 script ##########"
+sudo dnf install java-17-openjdk-devel -y
+echo java -version
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
+if [ -z "$JAVA_HOME" ]; then
+    echo "JAVA_HOME not found"
+else
+  echo "########## Java 17 installed ##########"
+fi
