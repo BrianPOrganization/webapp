a87cb3333ba27b70614f5295ed077422b5fc24b9
Brian Marcel Patrao <patrao.b@northeastern.edu>
2024-03-20 18:59:12 -0400

    Assignment 6

--- scripts/run-config.sh
+++ scripts/run-config.sh
@@ -19,5 +19,11 @@ echo "########## Adding ops agent ##########"
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 echo "########## moving the config.yml ##########"
-sudo cp /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
+if [ -f /tmp/config.yaml ]; then
+    echo "File exists"
+    ls -l /etc/google-cloud-ops-agent
+fi
+sudo cp -f /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
 sudo systemctl restart google-cloud-ops-agent
+sudo systemctl status google-cloud-ops-agent.service
+sudo journalctl -xe
