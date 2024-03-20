7857eb0e762338b64a3b017bc87014febbbdbeec
Brian Marcel Patrao <patrao.b@northeastern.edu>
2024-03-20 15:35:30 -0400

    application log correction

--- scripts/run-config.sh
+++ scripts/run-config.sh
@@ -6,8 +6,8 @@ sudo useradd -s /usr/sbin/nologin -g csye6225 -m csye6225
 sudo mkdir /opt/application
 sudo mv /tmp/application.service /etc/systemd/system/application.service
 echo "########## Logging configuration ##########"
-sudo touch /var/log/csye6225.log
-sudo chown -R csye6225:csye6225 /var/log/csye6225.log
+sudo touch /var/log/csye6225/application.log
+sudo chown -R csye6225:csye6225 /var/log/csye6225/application.log
 echo "########## Moving necessary files ##########"
 sudo mv /tmp/application-0.0.1-SNAPSHOT.jar /opt/application/application-0.0.1-SNAPSHOT.jar
 sudo chown -R csye6225:csye6225 /opt/application/application-0.0.1-SNAPSHOT.jar
